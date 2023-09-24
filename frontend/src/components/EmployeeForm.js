53f7ea931038614122ec2bc92684cca1359e7fe4
GitbitByKartik <130045099+GitbitByKartik@users.noreply.github.com>
2023-09-24 22:22:04 +0530

    Update EmployeeForm.js

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -32,14 +32,14 @@ const EmployeeForm = () => {
           <div>
             <label
               htmlFor="name"
-              className="text-base font-medium text-gray-900 dark:text-gray-200"
+              className="text-base font-medium text-gray-600 dark:text-gray-200"
             >
               {" "}
               Employee Name{" "}
             </label>
             <div className="mt-2.5">
               <input
-                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                className="text-white flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                 type="text"
                 placeholder="Enter You Full Name"
                 {...register("name")}
@@ -50,14 +50,14 @@ const EmployeeForm = () => {
           <div>
             <label
               htmlFor="email"
-              className="text-base font-medium text-gray-900 dark:text-gray-200"
+              className="text-base font-medium text-gray-600 dark:text-gray-200"
             >
               {" "}
               Employee Email Id{" "}
             </label>
             <div className="mt-2.5">
               <input
-                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                className="text-white flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                 type="email"
                 placeholder="Enter Your Email"
                 {...register("email")}
@@ -68,14 +68,14 @@ const EmployeeForm = () => {
           <div>
             <label
               htmlFor="title"
-              className="text-base font-medium text-gray-900 dark:text-gray-200"
+              className="text-base font-medium text-gray-600 dark:text-gray-200"
             >
               {" "}
               Employee Title{" "}
             </label>
             <div className="mt-2.5">
               <input
-                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                className="text-white flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                 type="text"
                 placeholder="Enter Your Employee Title"
                 {...register("title")}
@@ -86,14 +86,14 @@ const EmployeeForm = () => {
           <div>
             <label
               htmlFor="department"
-              className="text-base font-medium text-gray-900 dark:text-gray-200"
+              className="text-base font-medium text-gray-600 dark:text-gray-200"
             >
               {" "}
               Employee Department{" "}
             </label>
             <div className="mt-2.5">
               <input
-                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                className="text-white flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                 type="text"
                 placeholder="Enter Your Employee Department"
                 {...register("department")}
@@ -104,14 +104,14 @@ const EmployeeForm = () => {
           <div>
             <label
               htmlFor="role"
-              className="text-base font-medium text-gray-900 dark:text-gray-200"
+              className="text-base font-medium text-gray-600 dark:text-gray-200"
             >
               {" "}
               Employee Role{" "}
             </label>
             <div className="mt-2.5">
               <input
-                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                className="text-white flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                 type="text"
                 placeholder="Enter Your Employee Role"
                 {...register("role")}
